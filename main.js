af034f1fae87942bfe71563b98d29cd97c12c2ec
Shivangi Sood <sood.sh@northeastern.edu>
2022-02-13 01:10:45 -0500

    added event handling for shape 1

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function increment() {
+  var count = document.getElementById('counter').innerHTML;
+  ++count;
+  document.getElementById('counter').innerHTML = count;
+}
+
+function disappear() {
+    // Get svg class
+    let curr_visibility = document.getElementById("c3").getAttribute("class");
+
+    // Change visibility
+    if (curr_visibility == "appear") {
+        document.getElementById("c3").setAttribute("class", "disappear");
+    } else {
+        document.getElementById("c3").setAttribute("class", "appear");
+    }
